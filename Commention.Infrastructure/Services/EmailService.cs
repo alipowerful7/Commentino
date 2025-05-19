aee22afb607469a10626edb36110585ad059782c
Ali Khosrojerdi <ali7.khosrojerdi@gmail.com>
2025-05-19 23:58:56 +0330

    add: email service

---
+++ Commention.Infrastructure/Services/EmailService.cs
@@ -0,0 +1,51 @@
+﻿using Commention.Application.Interfaces;
+using MailKit.Net.Smtp;
+using MailKit.Security;
+using Microsoft.Extensions.Configuration;
+using MimeKit;
+
+namespace Commention.Infrastructure.Services
+{
+    public class EmailService : IEmailService
+    {
+        private readonly IConfiguration _configuration;
+
+        public EmailService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public async Task SendConfirmationEmailAsync(string email, string confirmationCode)
+        {
+            var emailSettings = _configuration.GetSection("EmailSettings");
+            string smtpServer = emailSettings["SmtpServer"]!;
+            int port = int.Parse(emailSettings["Port"]!);
+            string senderEmail = emailSettings["SenderEmail"]!;
+            string senderPassword = emailSettings["SenderPassword"]!;
+            bool enableSSL = bool.Parse(emailSettings["EnableSSL"]!);
+
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress("Commentino", senderEmail));
+
+            string recipientEmail = email;
+            message.To.Add(new MailboxAddress("", recipientEmail));
+
+            message.Subject = "کد تایید حساب کاربری";
+            message.Body = new TextPart("html")
+            {
+                Text = $"<h1>کد تایید شما:</h1><h3>{confirmationCode}</h3>"
+            };
+
+            using (var client = new SmtpClient())
+            {
+                await client.ConnectAsync(smtpServer, port, SecureSocketOptions.StartTls);
+
+                await client.AuthenticateAsync(senderEmail, senderPassword);
+
+                await client.SendAsync(message);
+
+                await client.DisconnectAsync(true);
+            }
+        }
+    }
+}
