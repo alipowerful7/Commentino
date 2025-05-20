b024a6f8248076302e41b1eb8a8e80cda063e149
Ali Khosrojerdi <ali7.khosrojerdi@gmail.com>
2025-05-20 21:49:03 +0330

    add: comment repository

---
+++ Commention.Infrastructure/Persistence/Repositories/CommentRepository.cs
@@ -0,0 +1,69 @@
+﻿using Commention.Domain.Interfaces;
+using Commention.Domain.Models.Entities;
+using Commention.Infrastructure.Persistence.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Commention.Infrastructure.Persistence.Repositories
+{
+    public class CommentRepository : ICommentRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CommentRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task ConfirmComment(long commentId)
+        {
+            var model = await GetCommentByIdAsync(commentId);
+            model.IsConfirm = true;
+            await UpdateCommentAsync(model);
+        }
+
+        public async Task CreateCommentAsync(Comment comment)
+        {
+            await _context.Comments.AddAsync(comment);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteCommentAsync(long commentId)
+        {
+            var model = await GetCommentByIdAsync(commentId);
+            _context.Comments.Remove(model);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<Comment>> GetAllCommentsAsync()
+        {
+            return await _context.Comments.Include(c => c.User).OrderBy(c => c.CreateDate).ToListAsync();
+        }
+
+        public async Task<Comment> GetCommentByIdAsync(long commentId)
+        {
+            var model = await _context.Comments.Include(c => c.User).FirstOrDefaultAsync(c => c.Id == commentId);
+            if (model == null)
+            {
+                throw new Exception("کامنت پیدا نشد.");
+            }
+            return model;
+        }
+
+        public async Task<IEnumerable<Comment>> GetCommentsByUserIdAsync(long userId)
+        {
+            return await _context.Comments.Where(c => c.UserId == userId).Include(c => c.User).OrderBy(c => c.CreateDate).ToListAsync();
+        }
+
+        public async Task<bool> IsCommentConfirm(long commentId)
+        {
+            var model = await GetCommentByIdAsync(commentId);
+            return model.IsConfirm;
+        }
+
+        public async Task UpdateCommentAsync(Comment comment)
+        {
+            _context.Comments.Update(comment);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
