72e0710d7ac1ae51bfc8f7bf2482d568129e7357
Ali Khosrojerdi <ali7.khosrojerdi@gmail.com>
2025-05-20 22:25:12 +0330

    modify: program.cs

---
+++ Commention.Api/Controllers/V1/CommentController.cs
@@ -0,0 +1,70 @@
+﻿using Commention.Application.Commands.Comment.ConfirmComment;
+using Commention.Application.Commands.Comment.CreateComment;
+using Commention.Application.Commands.Comment.DeleteComment;
+using Commention.Application.Commands.Comment.UpdateComment;
+using Commention.Application.Queries.Comment.GetAllComments;
+using Commention.Application.Queries.Comment.GetCommentById;
+using Commention.Application.Queries.Comment.GetCommentsByUserId;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Commention.Api.Controllers.V1
+{
+    [ApiVersion("1.0")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiController]
+    public class CommentController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public CommentController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [HttpPost("confirm-comment")]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> ConfirmComment(ConfirmCommentCommand command)
+        {
+            return Ok(await _mediator.Send(command));
+        }
+        [HttpPost("create-comment")]
+        [Authorize]
+        public async Task<IActionResult> CreateComment(CreateCommentCommand command)
+        {
+            var userId = long.Parse(User.FindFirst(System.Security.Claims.ClaimTypes.NameIdentifier)!.Value);
+            command.UserId = userId;
+            return Ok(await _mediator.Send(command));
+        }
+        [HttpDelete]
+        [Authorize]
+        public async Task<IActionResult> DeleteComment(DeleteCommentCommand command)
+        {
+            return Ok(await _mediator.Send(command));
+        }
+        [HttpPut]
+        [Authorize]
+        public async Task<IActionResult> UpdateComment(UpdateCommentCommand command)
+        {
+            return Ok(await _mediator.Send(command));
+        }
+        [HttpGet("get-all-comments")]
+        public async Task<IActionResult> GetAllComments()
+        {
+            return Ok(await _mediator.Send(new GetAllCommentsQuery()));
+        }
+        [HttpGet("{id}")]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> GetCommentById(long id)
+        {
+            return Ok(await _mediator.Send(new GetCommentByIdQuery { Id = id }));
+        }
+        [HttpGet("get-comments-by-user-id")]
+        [Authorize]
+        public async Task<IActionResult> GetCommentsByUserId()
+        {
+            var userId = long.Parse(User.FindFirst(System.Security.Claims.ClaimTypes.NameIdentifier)!.Value);
+            return Ok(await _mediator.Send(new GetCommentsByUserIdQuery { UserId = userId }));
+        }
+    }
+}
--- Commention.Api/Program.cs
+++ Commention.Api/Program.cs
@@ -1,3 +1,16 @@
+﻿using Commention.Application.Commands.PendUserRegister.CreatePendUserRegister;
+using Commention.Application.Common.Behaviours;
+using Commention.Application.Interfaces;
+using Commention.Domain.Interfaces;
+using Commention.Infrastructure.Persistence.Data;
+using Commention.Infrastructure.Persistence.Repositories;
+using Commention.Infrastructure.Services;
+using FluentValidation;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,11 +19,100 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+// Add DataBase Context
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Add Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddApiVersioning(options =>
+{
+    options.DefaultApiVersion = new Microsoft.AspNetCore.Mvc.ApiVersion(1, 0);
+    options.AssumeDefaultVersionWhenUnspecified = true;
+    options.ReportApiVersions = true;
+});
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Commentino Api",
+        Version = "v1",
+        Description = "Library Manager"
+    });
+
+    // اضافه کردن تعریف برای Authorization با JWT
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "Enter 'Bearer' followed by space and JWT token.\r\nExample: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6..."
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
+
+// Add MediatR
+builder.Services.AddMediatR(cfg =>
+{
+    cfg.RegisterServicesFromAssembly(typeof(CreatePendUserRegisterCommand).Assembly);
+    cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
+});
+
+// Add Authentication(Jwt)
+builder.Services.AddAuthentication(Microsoft.AspNetCore.Authentication.JwtBearer.JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Secret"]!))
+        };
+    });
+
+// Add FluentValidation
+builder.Services.AddValidatorsFromAssemblyContaining<Program>();
+builder.Services.AddValidatorsFromAssemblies(AppDomain.CurrentDomain.GetAssemblies());
+
+// Add Repositories
+builder.Services.AddScoped<IPendUserRegisterRepository, PendUserRegisterRepository>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<ICommentRepository, CommentRepository>();
+
+// Add Services
+builder.Services.AddScoped<IEmailService, EmailService>();
+builder.Services.AddScoped<IJwtService, JwtService>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "Commentino Api V1");
+        options.RoutePrefix = "swagger";
+    });
     app.MapOpenApi();
 }
 
